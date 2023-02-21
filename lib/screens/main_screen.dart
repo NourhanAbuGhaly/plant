cae0c8baa0cfd1362fe3ff0e58adb9fab0165dff
Nourhan Abu Ghaly <noorhanghaly2001@gmail.com>
2023-02-21 13:42:06 +0200

    plant

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -103,154 +103,160 @@ class StartedScreen extends StatefulWidget {
 class _StartedScreenState extends State<StartedScreen> {
   @override
   Widget build(BuildContext context) {
-    return Column(children: [
-      Container(
-        color: Color(0xffD0DFCF),
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: SizedBox(),
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        "TODAY TASKS",
-                        style: TextStyle(
-                            color: kMainColor,
-                            fontWeight: FontWeight.w100,
-                            fontSize: 10),
-                      ),
-                      Text(
-                        "Home",
-                        style: TextStyle(
-                            color: kMainColor,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 28),
-                      )
-                    ],
-                  ),
-                  IconButton(
-                    onPressed: () {},
-                    icon: Icon(
-                      Icons.settings_input_svideo,
-                      color: kMainColor,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Container(
-                    child: SingleChildScrollView(
-                      child: Stack(
-                        children: [
-                          Container(
-                            width: double.infinity,
-                            child: Image.asset(
-                              'assets/images/designecologist.png',
-                              fit: BoxFit.fitWidth,
-                            ),
-                          ),
-                          Container(
-                            width: double.infinity,
-                            child: Image.asset(
-                              'assets/images/finkelstein.png',
-                              fit: BoxFit.fitWidth,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: MediaQuery.of(context).size.height / 12,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                    child: Center(
-                      child: Column(
-                        children: [
-                          Text(
-                            "Identify and find new plants",
-                            style: TextStyle(
-                                color: kMainColor,
-                                fontWeight: FontWeight.w500,
-                                fontSize: 16),
-                          ),
-                          TextField(),
-                          SizedBox(
-                            height: 20,
-                          ),
-                          Text(
-                            "Get plant descriptions and care tips",
-                            style: TextStyle(
-                                color: KBlackColor,
-                                fontWeight: FontWeight.w200,
-                                fontSize: 16),
-                          ),
-
-                        ],
-                      ),
-                    ),
-                  )
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
-      Container(
-        decoration: BoxDecoration(
+    return SingleChildScrollView(
+      child: Column(children: [
+        Container(
           color: Kmainscreen,
-          borderRadius: BorderRadius.only(
-              bottomLeft: Radius.circular(200),
-              bottomRight: Radius.circular(200)),
-        ),
-        child: Column(
-          children: [
-            SizedBox(
-              height:
-              MediaQuery.of(context).size.height / 5,
-            ),
-            Center(
-              child: Padding(
-                padding: const EdgeInsets.only(bottom:12.0),
-                child: Icon(
-                  Icons.camera_alt_rounded,
-                  color: kMainColor,
-                  size: 40,
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: SizedBox(),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "TODAY TASKS",
+                          style: TextStyle(
+                              color: kMainColor,
+                              fontWeight: FontWeight.w100,
+                              fontSize: 10),
+                        ),
+                        Text(
+                          "Home",
+                          style: TextStyle(
+                              color: kMainColor,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 28),
+                        )
+                      ],
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.settings_input_svideo,
+                        color: kMainColor,
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            )
-          ],
-        ),
-      ),
-      Padding(
-        padding: const EdgeInsets.only(top: 16.0),
-        child: GestureDetector(
-          onTap: () {},
-          child: Text(
-            "OPEN CAMERA",
-            style: TextStyle(
-              color: kMainColor,
-              fontWeight: FontWeight.w700,
-              fontSize: 16,
-            ),
+              Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      child: SingleChildScrollView(
+                        child: Stack(
+                          children: [
+                            Container(
+                              width: double.infinity,
+                              child: Image.asset(
+                                'assets/images/designecologist.png',
+                                fit: BoxFit.fitWidth,
+                              ),
+                            ),
+                            Container(
+                              width: double.infinity,
+                              child: Image.asset(
+                                'assets/images/finkelstein.png',
+                                fit: BoxFit.fitWidth,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height / 12,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      child: Center(
+                        child: Column(
+                          children: [
+                            Text(
+                              "Identify and find new plants",
+                              style: TextStyle(
+                                  color: kMainColor,
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 16),
+                            ),
+                            TextField(),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            Text(
+                              "Get plant descriptions and care tips",
+                              style: TextStyle(
+                                  color: KBlackColor,
+                                  fontWeight: FontWeight.w200,
+                                  fontSize: 16),
+                            ),
+
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
           ),
         ),
-      )
-    ]);
+        Container(
+          decoration: BoxDecoration(
+            color: Kmainscreen,
+            borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(200),
+                bottomRight: Radius.circular(200)),
+          ),
+          child: Column(
+            children: [
+              SizedBox(
+                height:
+                MediaQuery.of(context).size.height / 7,
+              ),
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.only(bottom:10.0),
+                  child: CircleAvatar(
+                    maxRadius: 40.0,
+                    backgroundColor:kMainColor ,
+                    child: Icon(
+                      Icons.camera_alt_rounded,
+                      color: KTextFeild,
+                      size: 40,
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 16.0),
+          child: GestureDetector(
+            onTap: () {},
+            child: Text(
+              "OPEN CAMERA",
+              style: TextStyle(
+                color: kMainColor,
+                fontWeight: FontWeight.w700,
+                fontSize: 16,
+              ),
+            ),
+          ),
+        )
+      ]),
+    );
   }
 }
